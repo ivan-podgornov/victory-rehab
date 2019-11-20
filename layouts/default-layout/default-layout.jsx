5b273f222acafdd6127b5fa4a3874cec7575d0e6
Иван <34876346+ivan-podgornov@users.noreply.github.com>
2019-11-20 04:55:45 +0300

    chore: add yandex verification

--- layouts/default-layout/default-layout.jsx
+++ layouts/default-layout/default-layout.jsx
@@ -10,9 +10,10 @@ export default function DefaultLayout(props) {
                 <html lang="ru" />
                 <body className="page" />
                 <meta charSet="utf-8" />
-                <meta name="google-site-verification" content="It7DPRm4hPurH2gThL9xtdiF0Wvi8IBEUioWD8ntJww" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <meta name="google-site-verification" content="It7DPRm4hPurH2gThL9xtdiF0Wvi8IBEUioWD8ntJww" />
+                <meta name="yandex-verification" content="585ba55e83b5c817" />
                 <script type="text/javascript" >{`
                     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
